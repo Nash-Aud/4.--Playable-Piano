71521f49eef49dbed8ccfa44bde79533f8e1ddbb
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-03-10 12:34:42 -0300

    SetUp of the Piano keys

---
+++ script.js
@@ -0,0 +1,34 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key"),
+      volumeSlider = document.querySelectorAll(".piano-keys .key"),
+
+let allKeys = [],
+audio = new Audio("key_notes/a.wav"); // new Audio instance creates a HTML audio element
+
+const playTune = (key) => {
+    audio.src = `key_notes/${key}.wav`; //audio src based on key pressed
+    audio.play(); //playing the audio
+    
+    //getting the clicked key element
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active"); // add active class to each key element
+
+    // removing active class after 150 ms
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+}
+
+//looping through each key
+pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key); //adding data-key value to allKeys array
+
+    // calling playTune function with passing data-key value as an argument
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+
+const pressedKey = (e) => {
+    //if the pressed key is in the allKeys array, only call playTune function
+    if(allKeys.includes(e.key)) playTune(e.key);
+}
+document.addEventListener("keydown", pressedKey);
